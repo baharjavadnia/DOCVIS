b1d73a3cbbda5e91624be3e6fa7e2ecc96a3dda0
baharjavadnia <baharjvvd@gmail.com>
2024-06-02 23:29:58 +0330

    edited the css of articles, doctors carousel, and made some change in the whole appearance of the webpage

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var swiper = new Swiper(".slide-content", {
     slidesPerView: 4,
-    spaceBetween: 50,
+    spaceBetween: 30,
     loop: true,
     centerSlide: 'true',
     fade: 'true',
@@ -20,10 +20,10 @@ var swiper = new Swiper(".slide-content", {
             slidesPerView: 3,
         },
         520: {
-            slidesPerView: 3,
+            slidesPerView: 4,
         },
         950: {
-            slidesPerView: 3,
+            slidesPerView: 4,
         },
     },
   });
