3eb681c7637f8a563bed472634259d980f4e81c4
baharjavadnia <baharjvvd@gmail.com>
2024-06-03 16:01:55 +0330

    fixed the issues and bugs of otp form (resending code and limits of ints)

--- loginjava.js
+++ loginjava.js
@@ -1,8 +1,7 @@
 // Get the submit button element
 const submitButton = document.getElementById('submit-button');
-
-// Get all the tel input elements
-const telInputs = document.querySelectorAll('input[type="tel"]');
+const telInput = document.querySelector('input[type=tel]');
+const nameInput = document.querySelector('input[type=text')
 
 // Create the countdown timer element
 const countdownTimer = document.createElement('div');
@@ -18,75 +17,109 @@ submitButton.parentNode.insertBefore(countdownTimer, submitButton.nextSibling);
 // Set the initial input length for mobile number
 let inputLength = 11;
 
+let isSmsSend = false
+
+
 // Add an event listener to the tel input elements
-telInputs.forEach(input => {
-  input.addEventListener('keypress', function(e) {
-    // Check if the key pressed is a digit
-    if (/\d/.test(e.key)) {
-      // Check if the input value is less than 11 characters long
-      if (input.value.length < 11) {
-        // Allow the key to be entered
-        return true;
-      } else {
-        // Prevent the key from being entered
-        e.preventDefault();
-      }
-    } else {
-      // Prevent the key from being entered
-      e.preventDefault();
-    }
-  });
-});
+// telInputs.forEach(input => {
+//   input.addEventListener('keypress', function(e) {
+//     // Check if the key pressed is a digit
+//     if (/\d/.test(e.key)) {
+//       // Check if the input value is less than 11 characters long
+//       if (input.value.length < 11) {
+//         // Allow the key to be entered
+//         return true;
+//       } else {
+//         // Prevent the key from being entered
+//         e.preventDefault();
+//       }
+//     } else {
+//       // Prevent the key from being entered
+//       e.preventDefault();
+//     }
+//   });
+// });
+
 
 // Add an event listener to the submit button
 submitButton.addEventListener('click', function() {
-  // Loop through all the tel input elements
-  for (let i = 0; i < telInputs.length; i++) {
-    // Check if the input value is less than 11 digits long
-    if (telInputs[i].value.length < inputLength) {
-      // Show an error message
+
+  if(isSmsSend) {
+    if (telInput.value.length === 5) {
+      Swal.fire({
+        title: "خوش آمدید",
+        text: "",
+        icon: "success"
+      });
+    }
+    else {
       Swal.fire({
         title: "!هشدار",
-        text: "!لطفا شماره موبایل رو درست وارد کن",
+        text: "کد تایید رو به درستی وارد کن!",
         icon: "warning"
       });
-
-      // Break out of the loop
-      break;
-    } else {
-      // Change the submit button text to "Enter"
-      submitButton.innerHTML = 'ورود';
-
-      // Change the placeholder text to "Enter the verification code"
-      telInputs[i].placeholder = 'کد تایید را وارد کنید';
-
-      // Clear the input value
-      telInputs[i].value = '';
-
-      // Start the countdown timer
-      let timeLeft = 5;
-      const interval = setInterval(function() {
-        if (timeLeft <= 0) {
-          // Stop the countdown timer
-          clearInterval(interval);
-
-          // Show the "Didn't get the code? Resend" text
-          countdownTimer.innerHTML = 'کد تایید را دریافت نکردید؟ <a href="#">ارسال مجدد</a>';
-          interval = window.setInterval(interval, 4000);
-
-        } else {
-          // Update the countdown timer
-          countdownTimer.innerHTML = 'زمان باقی مانده : ' + timeLeft;
-          timeLeft -= 1;
-        }
-      }, 1000);
-      
-
-      // Break out of the loop
-      break;
     }
   }
+  else {
+        const nameCheckRegex = /^[A-Za-z\u0600-\u06FF\s]+$/
+
+        if(!nameCheckRegex.test(nameInput.value)) {
+          Swal.fire({
+            title: "!هشدار",
+            text: "فرمت نام و نام خانوادگی اشتباه است!",
+            icon: "warning"
+          });
+        }
+        // Check if the input value is less than 11 digits long
+        else if (telInput.value.length < inputLength) {
+          // Show an error message
+          Swal.fire({
+            title: "!هشدار",
+            text: "!لطفا شماره موبایل رو درست وارد کن",
+            icon: "warning"
+          });
+        } 
+        else {
+          // Change the submit button text to "Enter"
+          submitButton.innerHTML = 'ورود';
+    
+          // Change the placeholder text to "Enter the verification code"
+          telInput.placeholder = 'کد تایید را وارد کنید';
+    
+          // Clear the input value
+          telInput.value = '';
+    
+          sendSMS();
+        }
+  }
 });
+
+function sendSMS() {
+  isSmsSend = true;
+
+  // Start the countdown timer
+  let timeLeft = 5;
+  const interval = setInterval(function() {
+    if (timeLeft <= 0) {
+      // Stop the countdown timer
+      clearInterval(interval);
+
+      // Show the "Didn't get the code? Resend" text
+      countdownTimer.innerHTML = 'کد تایید را دریافت نکردید؟ <a href="#">ارسال مجدد</a>';
+
+      countdownTimer.querySelector('a').addEventListener('click', () => {
+        sendSMS();
+      })
+
+      interval = window.setInterval(interval, 4000);
+
+    } else {
+      // Update the countdown timer
+      countdownTimer.innerHTML = 'زمان باقی مانده : ' + timeLeft;
+      timeLeft -= 1;
+    }
+  }, 1000);
+}
 
 // Define the interval variable outside of the event listeners
 let interval;
